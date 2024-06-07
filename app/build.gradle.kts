be1d4833e52016ba7e00258f506f820a1b74b14c
Federico Giovanni Cardara <federico.cardara@uvic.cat>
2024-06-07 12:00:36 +0200

    add jdbc dependency to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 dependencies {
     implementation(project(":domain"))
     implementation(project(":domain-implementations:jpa"))
+    implementation(project(":domain-implementations:jdbc"))
 
     implementation("com.github.freva:ascii-table:1.8.0")
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
