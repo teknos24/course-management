eaf1befc95f8ec388aef302872314514f86f44b8
Federico Cardara <federico.cardara@uvic.cat>
2024-10-04 19:24:04 +0200

    Example of Router and controller implementation

--- services/src/main/java/cat/uvic/teknos/coursemanagement/services/RequestRouter.java
+++ services/src/main/java/cat/uvic/teknos/coursemanagement/services/RequestRouter.java
@@ -1,7 +1,11 @@
 package cat.uvic.teknos.coursemanagement.services;
 
+import cat.uvic.teknos.coursemanagement.models.Student;
+import cat.uvic.teknos.coursemanagement.services.controllers.StudentController;
 import cat.uvic.teknos.coursemanagement.services.exception.ResourceNotFoundException;
 import cat.uvic.teknos.coursemanagement.services.exception.ServerErrorException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import rawhttp.core.RawHttp;
 import rawhttp.core.RawHttpOptions;
 import rawhttp.core.RawHttpRequest;
@@ -19,16 +23,28 @@ public class RequestRouter {
     public RawHttpResponse<?> execRequest(RawHttpRequest request) {
         var path = request.getUri().getPath();
         var method = request.getMethod();
+        var pathParts = path.split("/");
+        var controllerName = pathParts[1];
+        var responseJsonBody = "";
+        
+        switch (controllerName) {
+            case "courses":
+                responseJsonBody = manageStudents(request, method, pathParts, responseJsonBody);
+                break;
+
+        }
 
         RawHttpResponse response = null;
         try {
             // TODO: Router logic
-            var json =  ""; //studentController.get();
+
+
+            
             response = rawHttp.parseResponse("HTTP/1.1 200 OK\r\n" +
                     "Content-Type: text/json\r\n" +
-                    "Content-Length: " + json.length() + "\r\n" +
+                    "Content-Length: " + responseJsonBody.length() + "\r\n" +
                     "\r\n" +
-                    json);
+                    responseJsonBody);
         } catch (ResourceNotFoundException exception) {
             response = null;
         } catch (ServerErrorException exception) {
@@ -36,5 +52,37 @@ public class RequestRouter {
         }
 
         return null;
+    }
+
+    private static String manageStudents(RawHttpRequest request, String method, String[] pathParts, String responseJsonBody) {
+        var controller = new StudentController();
+
+        if (method == "POST") {
+            var studentJson = request.getBody().get().toString();
+            var mapper = new ObjectMapper();
+            try {
+                var student = mapper.readValue(studentJson, Student.class);
+                controller.post(student);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (method == "GET" && pathParts.length == 2) {
+            responseJsonBody = controller.get();
+
+        } else if (method == "DELETE") {
+            var studentId = Integer.parseInt(pathParts[2]);
+            controller.delete(studentId);
+        } else if (method == "PUT") {
+            var studentId = Integer.parseInt(pathParts[2]);
+            var mapper = new ObjectMapper();
+            try {
+                var studentJson = request.getBody().get().toString();
+                var student = mapper.readValue(studentJson, Student.class);
+                controller.put(studentId, student);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return responseJsonBody;
     }
 }
--- services/src/main/java/cat/uvic/teknos/coursemanagement/services/controllers/StudentController.java
+++ services/src/main/java/cat/uvic/teknos/coursemanagement/services/controllers/StudentController.java
@@ -1,9 +1,12 @@
 package cat.uvic.teknos.coursemanagement.services.controllers;
 
 import cat.uvic.teknos.coursemanagement.models.Student;
+import cat.uvic.teknos.coursemanagement.repositories.StudentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class StudentController implements Controller<Integer, Student>{
+    StudentRepository repository;
     @Override
     public String get(Integer integer) {
         // retrieve (get) student from db
@@ -14,12 +17,20 @@ public class StudentController implements Controller<Integer, Student>{
 
     @Override
     public String get() {
-        return null;
+        var students = repository.getAll();
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(students);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void post(Student value) {
-
+        repository.save(value);
     }
 
     @Override
