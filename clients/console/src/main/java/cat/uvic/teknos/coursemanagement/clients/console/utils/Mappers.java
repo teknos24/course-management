fd6a20992c29f84d9ba6a6bdeb82481314f5c1ff
Federico Cardara <federico.cardara@uvic.cat>
2024-11-08 19:51:39 +0100

    Code refactoring

--- clients/console/src/main/java/cat/uvic/teknos/coursemanagement/clients/console/utils/Mappers.java
+++ clients/console/src/main/java/cat/uvic/teknos/coursemanagement/clients/console/utils/Mappers.java
@@ -10,12 +10,17 @@ public class Mappers {
     private static final ObjectMapper mapper;
 
     static  {
-        final var genreTypeMapping = new SimpleModule()
-                .addAbstractTypeMapping(Genre.class, GenreDto.class);
         mapper = new ObjectMapper();
         mapper
                 .registerModule(new JavaTimeModule()) // Registered to map LocalDate (add implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.0" to build.gradle.kts) )
-                .registerModule(genreTypeMapping); // Registered to map the Genre deserialization
+                .registerModule(
+                        new SimpleModule()
+                            .addAbstractTypeMapping(Genre.class, GenreDto.class)
+                )
+                .registerModule(
+                        new SimpleModule()
+                                .addAbstractTypeMapping(Genre.class, GenreDto.class)
+                ); // Registered to map the Genre deserialization
     }
 
     public static ObjectMapper get() {
